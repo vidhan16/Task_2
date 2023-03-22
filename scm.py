743422da2447cf6daf875361afcb33e976e6739f
vibhorverma24 <vibhorverma8790@gmail.com>
2023-03-22 19:14:30 +0530

    file

--- scm.py
+++ scm.py
@@ -42,3 +42,62 @@ Log.fit(X_train,y_train)
 y_pred1 = Log.predict(X_test)
 from sklearn.metrics import accuracy_score
 accuracy_score(y_test,y_pred1)
+from sklearn import svm
+svm = svm.SVC()
+svm.fit(X_train,y_train)
+y_pred2 = svm.predict(X_test)
+accuracy_score(y_test,y_pred2)
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(X_train,y_train)
+KNeighborsClassifier()
+y_pred3=knn.predict(X_test)
+accuracy_score(y_test,y_pred3)
+score = []  
+for k in range(1,40):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train,y_train)
+    y_pred=knn.predict(X_test)
+    score.append(accuracy_score(y_test,y_pred))
+score
+knn = KNeighborsClassifier(n_neighbors=2)
+knn.fit(X_train,y_train)
+y_pred=knn.predict(X_test)
+accuracy_score(y_test,y_pred)
+data =pd.read_csv('heart.csv')
+data.head()
+data = data.drop_duplicates()
+data.shape
+X = data.drop('target',axis=1)
+y = data['target']
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+from sklearn.tree import DecisionTreeClassifier
+dt = DecisionTreeClassifier()
+dt.fit(X_train,y_train)
+y_pred4 = dt.predict(X_test)
+accuracy_score(y_test,y_pred4)
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier()
+rf.fit(X_train,y_train)
+y_pred5= rf.predict(X_test)
+accuracy_score(y_test,y_pred5)
+from sklearn.ensemble import GradientBoostingClassifier
+gbc = GradientBoostingClassifier()
+gbc.fit(X_train,y_train)
+y_pred6 = gbc.predict(X_test)
+accuracy_score(y_test,y_pred6)
+final_data = pd.DataFrame({'Models':['LR','SVM','KNN','DT','RF','GB'],
+                          'ACC':[accuracy_score(y_test,y_pred1),
+                                accuracy_score(y_test,y_pred2),
+                                accuracy_score(y_test,y_pred3),
+                                accuracy_score(y_test,y_pred4),
+                                accuracy_score(y_test,y_pred5),
+                                accuracy_score(y_test,y_pred6)]})
+final_data
+import seaborn as sns
+X = data.drop('target',axis=1)
+y = data['target']
+X.shape
+from sklearn.ensemble import  RandomForestClassifier
+rf =  RandomForestClassifier()
+rf.fit(X,y)
